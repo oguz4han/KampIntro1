3ef0aaa36131e21a9109d3f8659494c389f1565a
Oğuzhan ÇAKMAK <83668054+oguz4han@users.noreply.github.com>
2024-07-25 16:11:57 +0300

    denem

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+
+        public void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine($"{musteri.name} adlı musteri eklendi.");
+        }
+
+        public void MusteriListeleme(Musteri musteri)
+        {
+            Console.WriteLine("Musteri listelendi.");
+        }
+
+        public void MusteriSilindi(Musteri musteri)
+        {
+            Console.WriteLine( musteri.name + " adlı musteri silindi");
+        }
+
+
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassMetotDemo;
+
+Console.WriteLine("Hello, World!");
+
+/*
+ * 
+Projeniz şunu yapacak.
+
+Bir bankada müşteri takibi yapmak istiyorsunuz.
+Musteri isimli bir Class oluşturunuz. Müşteriye istediğiniz özellikleri ekleyiniz. (Id,Ad,Soyad....)
+MusteriManager sınıfı oluşturunuz. Musteri parametresi alarak Musteri ekleme, listeleme,silme gibi metotları simule ediniz.
+
+*/
+
+
+
+Musteri musteri1 = new Musteri();
+musteri1.Id = 1;
+musteri1.name = "Oğuzhan";
+musteri1.lastName = "Çakmak";
+
+MusteriManager manager = new MusteriManager();
+manager.MusteriEkleme(musteri1);
